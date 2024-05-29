de329bf1600e33965b17c339a5778f8d96fc65df
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-05-29 19:53:49 +0900

    Remove unnecessary content from Hero component

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -3,7 +3,7 @@ import Container from "../layout/container";
 const Hero = () => {
   return (
     <section className="hero bg-HERO_PATTERN">
-      <Container className="size-full h-screen flex items-center justify-center">asdasd</Container>
+      <Container className=""></Container>
     </section>
   );
 };
