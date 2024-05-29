2e65a6712875e1294919601f39dc4e0bc1af4d06
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-05-29 19:42:50 +0900

    fonts configured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,27 @@
 import "./globals.css";
 
-import { Inter } from "next/font/google";
 import Loader from "@/components/sections/loader";
 import Provider from "@/config/provider";
+import { Viewport } from "next";
+import { colors } from "@/config/colors";
 import { getSEOTags } from "@/lib/seo";
+import localFont from "next/font/local";
 
-const font = Inter({ subsets: ["latin"] });
+const font = localFont({
+  src: [
+    { path: "../../../public/fonts/OverusedGrotesk-Bold.otf", weight: "700" },
+    { path: "../../../public/fonts/OverusedGrotesk-Medium.otf", weight: "500" },
+    { path: "../../../public/fonts/OverusedGrotesk-VF.otf", weight: "400" },
+  ],
+  display: "block",
+});
+
+export const viewport: Viewport = {
+  // Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
+  themeColor: colors.main,
+  width: "device-width",
+  initialScale: 1,
+};
 
 export const metadata = getSEOTags();
 
@@ -16,7 +32,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${font.className} bg-BACKGROUND text-PRIMARY w-screen h-screen`}>
+      <body className={`${font.className} bg-BACKGROUND text-PRIMARY`}>
         <Provider>
           <Loader />
           {children}
