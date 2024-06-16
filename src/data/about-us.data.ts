417e91a74f59c53f9134ed521c2f4b2fc3b1f00d
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-16 16:43:40 +0500

    added about us data for maping

---
+++ src/data/about-us.data.ts
@@ -0,0 +1,29 @@
+export const aboutData = [
+  {
+    title: 'Go beyond limitations',
+    text: `
+    We offer a wider range of funding solutions than traditional lenders. This means exploring business credit building, lines of credit, and alternative funding sources to fit your specific needs, whether you're launching a startup or scaling an existing business.
+    `,
+    img: 'expand',
+  },
+  {
+    title: 'Expert Guidance',
+    text: `
+    We offer a wider range of funding solutions than traditional lenders. This means exploring business credit building, lines of credit, and alternative funding sources to fit your specific needs, whether you're launching a startup or scaling an existing business.
+    `,
+    img: 'navigation',
+  },
+  {
+    title: 'Financial Literacy is Key',
+    text: `
+    TruDream believes that knowledge is power.  That's why we don't just provide funding; we equip you with the financial literacy tools to manage your money effectively.    `,
+    img: 'book',
+  },
+  {
+    title: 'Personalized Education',
+    text: `
+    TruDream believes that knowledge is power.  That's why we don't just provide funding; we equip you with the financial literacy tools to manage your money effectively.
+    `,
+    img: 'chat',
+  },
+];
