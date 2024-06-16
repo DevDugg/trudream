7934491db3367125955fbee0ab12552a57c5b34a
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-16 17:04:24 +0500

    business section mobile ready

---
+++ src/components/sections/business/business.tsx
@@ -0,0 +1,38 @@
+import Container from '@/components/layout/container';
+import { Button } from '@/components/ui/button';
+import { businessData } from '@/data/business.data';
+import Image from 'next/image';
+
+const Business = () => {
+  return (
+    <section>
+      <Container>
+        <div className="flex flex-col gap-20">
+          <h3 className="leading-none text-[24px] font-bold">
+            Funding Solutions & Financial Education by TruDream
+          </h3>
+
+          <div className="flex flex-col gap-10">
+            {businessData.map((item) => (
+              <div>
+                <h2 className="text-[32px] font-bold leading-none text-BLACK mb-6">{item.title}</h2>
+                <Image
+                  src={`/images/business/${item.img}.png`}
+                  alt="image"
+                  width={420}
+                  height={200}
+                  className="mb-3"
+                />
+                <p className="leading-[140%] text-BLACK">{item.text}</p>
+              </div>
+            ))}
+          </div>
+
+          <Button className="text-[18px] font-medium">Book a call</Button>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default Business;
