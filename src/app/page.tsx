4675138ffaa5dfdf18cca44cbf163863f11fd772
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-16 17:03:49 +0500

    added Business section in the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import AboutUs from '@/components/sections/about-us/about-us';
+import Business from '@/components/sections/business/business';
 import Hero from '@/components/sections/hero/hero';
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <main className="home flex flex-col gap-[100px] py-[60px]">
       <Hero />
       <AboutUs />
+      <Business />
     </main>
   );
 }
