2b6812d3c6d4f7fe4041a3247db268626d90f010
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-16 17:57:15 +0500

    added data for reviews

---
+++ src/data/reviews.data.ts
@@ -0,0 +1,32 @@
+export const reviewsData = [
+  {
+    name: 'This is Elon Musk',
+    email: '@stevejobs',
+    text: 'Lorem ipsum dolor sit amet consectetur. Vitae purus cursus mollis erat. Velit quis et dui non blandit. Lorem ipsum dolor sit amet consectetur. Velit quis et dui non blandit. Lorem ipsum dolor sit amet consectetur.  Velit quis et dui non blandit. ',
+    stars: ['', '', '', '', ''],
+  },
+  {
+    name: 'This is Elon Musk',
+    email: '@stevejobs',
+    text: 'Lorem ipsum dolor sit amet consectetur. Vitae purus cursus mollis erat. Velit quis et dui non blandit. Lorem ipsum dolor sit amet consectetur. Velit quis et dui non blandit. Lorem ipsum dolor sit amet consectetur.  Velit quis et dui non blandit. ',
+    stars: ['', '', '', '', ''],
+  },
+  {
+    name: 'This is Elon Musk',
+    email: '@stevejobs',
+    text: 'Lorem ipsum dolor sit amet consectetur. Vitae purus cursus mollis erat. Velit quis et dui non blandit. Lorem ipsum dolor sit amet consectetur. Velit quis et dui non blandit. Lorem ipsum dolor sit amet consectetur.  Velit quis et dui non blandit. ',
+    stars: ['', '', '', '', ''],
+  },
+  {
+    name: 'This is Elon Musk',
+    email: '@stevejobs',
+    text: 'Lorem ipsum dolor sit amet consectetur. Vitae purus cursus mollis erat. Velit quis et dui non blandit. Lorem ipsum dolor sit amet consectetur. Velit quis et dui non blandit. Lorem ipsum dolor sit amet consectetur.  Velit quis et dui non blandit. ',
+    stars: ['', '', '', '', ''],
+  },
+  {
+    name: 'This is Elon Musk',
+    email: '@stevejobs',
+    text: 'Lorem ipsum dolor sit amet consectetur. Vitae purus cursus mollis erat. Velit quis et dui non blandit. Lorem ipsum dolor sit amet consectetur. Velit quis et dui non blandit. Lorem ipsum dolor sit amet consectetur.  Velit quis et dui non blandit. ',
+    stars: ['', '', '', '', ''],
+  },
+];
