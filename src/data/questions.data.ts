26e7e2c5e6e6a8890f693ff9de94a2808c5ecee1
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-16 19:06:05 +0500

    added questions data

---
+++ src/data/questions.data.ts
@@ -0,0 +1,17 @@
+export const questionsData = [
+  {
+    title: 'What funding options do you offer?',
+  },
+  {
+    title: 'Do I need good credit to qualify for your services?',
+  },
+  {
+    title: 'What is the cost of your services?',
+  },
+  {
+    title: 'How long does it take to secure funding?',
+  },
+  {
+    title: 'Do you offer any guarantees on securing funding?',
+  },
+];
