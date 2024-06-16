4405796fa948ad7b91dba10a371468c572446f51
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-16 16:42:12 +0500

    added about-us in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Hero from "@/components/sections/hero/hero";
+import AboutUs from '@/components/sections/about-us/about-us';
+import Hero from '@/components/sections/hero/hero';
 
 export default function Home() {
   return (
     <main className="home flex flex-col gap-[100px] py-[60px]">
       <Hero />
+      <AboutUs />
     </main>
   );
 }
