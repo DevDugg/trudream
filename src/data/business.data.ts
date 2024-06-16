abdc4c2fc27eca8863530f1adb6153b9f38a9ab9
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-16 17:04:39 +0500

    added data for business section

---
+++ src/data/business.data.ts
@@ -0,0 +1,32 @@
+export const businessData = [
+  {
+    title: 'Business Credit Stacking',
+    text: 'Secure the capital you need to launch, scale, or invest in your business dreams. We navigate traditional and alternative funding options like business credit building, lines of credit, and grant applications to find the perfect fit.',
+    img: '1',
+  },
+  {
+    title: 'Business Lines of Credit',
+    text: 'Secure the capital you need to launch, scale, or invest in your business dreams. We navigate traditional and alternative funding options like business credit building, lines of credit, and grant applications to find the perfect fit.',
+    img: '2',
+  },
+  {
+    title: 'Business Term Loans',
+    text: 'Secure the capital you need to launch, scale, or invest in your business dreams. We navigate traditional and alternative funding options like business credit building, lines of credit, and grant applications to find the perfect fit.',
+    img: '3',
+  },
+  {
+    title: 'Real Estate Financing',
+    text: 'Secure the capital you need to launch, scale, or invest in your business dreams. We navigate traditional and alternative funding options like business credit building, lines of credit, and grant applications to find the perfect fit.',
+    img: '4',
+  },
+  {
+    title: 'Investment Capital Financing',
+    text: 'Secure the capital you need to launch, scale, or invest in your business dreams. We navigate traditional and alternative funding options like business credit building, lines of credit, and grant applications to find the perfect fit.',
+    img: '5',
+  },
+  {
+    title: 'Financial Literacy Education',
+    text: 'Secure the capital you need to launch, scale, or invest in your business dreams. We navigate traditional and alternative funding options like business credit building, lines of credit, and grant applications to find the perfect fit.',
+    img: '6',
+  },
+];
