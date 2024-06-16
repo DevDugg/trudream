736bace38e8395e2b390f310b08e947c6e5e6b75
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-16 17:57:07 +0500

    added data for guide section

---
+++ src/data/guide.data.ts
@@ -0,0 +1,18 @@
+export const guideData = [
+  {
+    title: '1.  Free Consultation',
+    text: 'Get started with a free, no-obligation consultation.  Our dedicated team will take the time to understand your business goals, financial situation, and funding needs. This initial discussion allows us to tailor our approach to best serve you.',
+  },
+  {
+    title: '2.  Personalized Strategy',
+    text: 'Based on your consultation, we develop a customized strategy to achieve your financial objectives. This plan may include exploring funding options, credit score optimization, and creating a roadmap for building financial literacy.',
+  },
+  {
+    title: '3.  Funding Acquisition Support',
+    text: 'We guide you through the funding process, navigating traditional and alternative options.  Our experts will help you craft compelling applications and increase your chances of securing the capital you need.',
+  },
+  {
+    title: '4.  Financial Education',
+    text: 'TruDream goes beyond just funding. We equip you with the financial knowledge and tools you need to manage your money effectively.  Access our library of resources, workshops, and personalized coaching to become financially savvy.',
+  },
+];
