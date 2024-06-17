ff5bc0ba23c66566d582c5dcbcb10311da8a1e5b
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-17 15:57:09 +0500

    added payment icons

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -1,6 +1,8 @@
-import Container from "../../layout/container";
-import HeroForm from "./hero-form";
-import { general } from "@/config/general";
+import Image from 'next/image';
+import Container from '../../layout/container';
+import HeroForm from './hero-form';
+import { general } from '@/config/general';
+import { heroPayData } from '@/data/hero.data';
 
 const Hero = () => {
   return (
@@ -9,11 +11,27 @@ const Hero = () => {
         <div className="flex flex-col gap-4">
           <h1 className="text-[32px] font-bold leading-none">{general.appDescription}</h1>
           <p className="text-BLACK text-base">
-            We empowers business owners like you to turn dreams into reality. We provide funding and financial literacy
-            education to help you launch, scale, or invest in your business goals.
+            We empowers business owners like you to turn dreams into reality. We provide funding and
+            financial literacy education to help you launch, scale, or invest in your business
+            goals.
           </p>
         </div>
         <HeroForm />
+
+        <div className="flex flex-col gap-4">
+          <h4 className="h4">Invested in</h4>
+          <div className="flex gap-5">
+            {heroPayData.map((item) => (
+              <Image
+                key={item.path}
+                src={`/images/hero/${item.path}.svg`}
+                alt="icon"
+                width={40}
+                height={40}
+              />
+            ))}
+          </div>
+        </div>
       </Container>
     </section>
   );
