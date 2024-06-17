7cb9c1c9b208f2cab8a120c4fcdc15cd41ea9bbd
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-17 15:57:22 +0500

    data for payment icons

---
+++ src/data/hero.data.ts
@@ -0,0 +1,20 @@
+export const heroPayData = [
+  {
+    path: '1',
+  },
+  {
+    path: '2',
+  },
+  {
+    path: '3',
+  },
+  {
+    path: '4',
+  },
+  {
+    path: '5',
+  },
+  {
+    path: '6',
+  },
+];
