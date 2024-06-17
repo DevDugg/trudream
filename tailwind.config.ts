0d136ac251827363da395e06cbd12c744e43194a
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-17 15:56:08 +0500

    pattern for help section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,42 +1,48 @@
-import type { Config } from "tailwindcss";
-import { colors } from "./src/config/colors";
-import { ui } from "./src/config/ui";
+import type { Config } from 'tailwindcss';
+import { colors } from './src/config/colors';
+import { ui } from './src/config/ui';
 
 const config = {
-  darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-  prefix: "",
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  prefix: '',
   theme: {
     container: {
       center: true,
       padding: {
-        "2xl": ui.container.desktopPadding,
+        '2xl': ui.container.desktopPadding,
         lg: ui.container.tabletPadding,
         md: ui.container.mobilePadding,
       },
     },
     extend: {
       keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
       },
       animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
       colors: colors,
       backgroundImage: {
         HERO_PATTERN: "url('/images/hero-pattern.png')",
+        HELP_PATTERN: "url('/images/help-pattern.png')",
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
 
 export default config;
