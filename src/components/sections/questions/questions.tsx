ea66993cbebb164852293e46f47e324735abe76b
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-17 15:14:56 +0500

    faq animation is ready

--- src/components/sections/questions/questions.tsx
+++ src/components/sections/questions/questions.tsx
@@ -26,17 +26,20 @@ const Questions = () => {
         <div className="flex flex-col gap-6">
           {questionsData.map((item) => (
             <motion.div
+              initial={{ height: 0 }}
+              animate={{ height: '100%' }}
+              exit={{ height: 0 }}
               onClick={() => onFAQ(item.title)}
               key={item.title}
               className={clsx(
-                'flex flex-col text-BLACK transition-all rounded-[16px] cursor-pointer bg-LIGHT_GRAY border-GRAY border-[1px] font-semibold leading-[120%] py-[18.5px] px-4',
+                'flex flex-col text-BLACK rounded-[16px] transition-all cursor-pointer bg-LIGHT_GRAY border-GRAY border-[1px] font-semibold leading-[120%] py-[18.5px] px-4',
                 {
                   'border-none': activeFAQ === item.title,
                 },
               )}>
               <div className="flex items-center gap-6 justify-between">
                 <h4
-                  className={clsx({
+                  className={clsx('transition-all', {
                     'text-ACCENT': activeFAQ === item.title,
                   })}>
                   {item.title}
@@ -46,14 +49,22 @@ const Questions = () => {
                   alt="plus"
                   width={24}
                   height={24}
+                  className="transition-all"
                 />
               </div>
 
               <AnimatePresence>
                 {activeFAQ === item.title && (
-                  <motion.p className="mt-6 text-[16px] leading-[140%] font-normal">
-                    {item.text}
-                  </motion.p>
+                  <motion.div
+                    key="content"
+                    initial={{ opacity: 0, height: 0 }}
+                    animate={{ opacity: 1, height: 'auto' }}
+                    exit={{ opacity: 0, height: 0 }}
+                    transition={{ duration: 0.3 }}>
+                    <p className="mt-6 text-[16px] transition-all leading-[140%] font-normal">
+                      {item.text}
+                    </p>
+                  </motion.div>
                 )}
               </AnimatePresence>
             </motion.div>
