3ef38808dd4c285a2fb29e64da6fa949f4a60244
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-17 18:39:47 +0500

    nothing

--- src/components/sections/header/burger.tsx
+++ src/components/sections/header/burger.tsx
@@ -1,10 +1,10 @@
-"use client";
+'use client';
 
-import LinkExternal, { LinkExternalProps } from "../link-external";
+import LinkExternal, { LinkExternalProps } from '../link-external';
 
-import BurgerLinkInternal from "./burger-internal-link";
-import { motion } from "framer-motion";
-import { socialLinks } from "../footer/footer";
+import BurgerLinkInternal from './burger-internal-link';
+import { motion } from 'framer-motion';
+import { socialLinks } from '../footer/footer';
 
 type BurgerProps = {
   isOpened: boolean;
@@ -12,24 +12,24 @@ type BurgerProps = {
 
 export const headerLinks: LinkExternalProps[] = [
   {
-    name: "About",
-    href: "#about",
+    name: 'About',
+    href: '#about',
   },
   {
-    name: "How it works",
-    href: "#how-it-works",
+    name: 'How it works',
+    href: '#how-it-works',
   },
   {
-    name: "Testimonials",
-    href: "#testimonials",
+    name: 'Testimonials',
+    href: '#testimonials',
   },
   {
-    name: "FAQ",
-    href: "#faq",
+    name: 'FAQ',
+    href: '#faq',
   },
   {
-    name: "Contact",
-    href: "#contact",
+    name: 'Contact',
+    href: '#contact',
   },
 ];
 
@@ -37,9 +37,8 @@ const Burger = ({ isOpened }: BurgerProps) => {
   return (
     <motion.div
       className="burger fixed top-[52px] left-0 w-screen h-[calc(100dvh-52px)] px-4 py-20 flex flex-col gap-6 z-50 bg-WHITE"
-      initial={{ opacity: 0, x: "100%" }}
-      animate={isOpened ? { opacity: 1, x: "0%" } : {}}
-    >
+      initial={{ opacity: 0, x: '100%' }}
+      animate={isOpened ? { opacity: 1, x: '0%' } : {}}>
       <ul className="links w-full flex flex-col gap-6">
         {headerLinks.map((link, i) => (
           <li key={i}>
