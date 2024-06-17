714d5dc1d5b530950d1061521fd7e565f82c41cd
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-17 17:21:30 +0500

    tabl is ready

--- src/components/sections/guide/guide.tsx
+++ src/components/sections/guide/guide.tsx
@@ -6,10 +6,16 @@ const Guide = () => {
   return (
     <section>
       <Container>
-        <h3 className="h3 mb-10">A Step-by-Step Guide to TruDream</h3>
-        <Image src={'/images/guide/1.png'} alt="image" width={400} height={450} className="mb-6" />
+        <h3 className="h3 mb-10 sm:text-center">A Step-by-Step Guide to TruDream</h3>
+        <Image
+          src={'/images/guide/3.png'}
+          alt="image"
+          width={1000}
+          height={1000}
+          className="mb-6 sm:mb-8 overflow-hidden h-[380px] sm:h-[400px] w-full object-cover"
+        />
 
-        <div className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
           {guideData.map((item) => (
             <div key={item.title} className="flex flex-col gap-4">
               <h4 className="text-ACCENT text-[18px] font-bold leading-none">{item.title}</h4>
