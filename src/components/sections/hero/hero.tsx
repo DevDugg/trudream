380a764995efb5b2ef76f6d3565effee7e84db32
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-17 17:22:39 +0500

    tabl is ready

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -7,10 +7,12 @@ import { heroPayData } from '@/data/hero.data';
 const Hero = () => {
   return (
     <section className="hero bg-HERO_PATTERN">
-      <Container className="flex flex-col gap-10">
-        <div className="flex flex-col gap-4">
-          <h1 className="text-[32px] font-bold leading-none">{general.appDescription}</h1>
-          <p className="text-BLACK text-base">
+      <Container className="flex flex-col gap-10 sm:gap-8">
+        <div className="flex flex-col gap-4 sm:text-center">
+          <h1 className="text-[32px] sm:text-[40px] font-bold leading-none">
+            {general.appDescription}
+          </h1>
+          <p className="text-BLACK text-base sm:text-[18px]">
             We empowers business owners like you to turn dreams into reality. We provide funding and
             financial literacy education to help you launch, scale, or invest in your business
             goals.
@@ -19,7 +21,7 @@ const Hero = () => {
 
         <HeroForm />
 
-        <div className="flex flex-col gap-4">
+        <div className="flex flex-col sm:items-center gap-4 sm:gap-6">
           <h4 className="h4">Invested in</h4>
           <div className="flex gap-5">
             {heroPayData.map((item) => (
