6c5fca698937bd760e4ba58af3113786367cbc46
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-17 18:40:00 +0500

    desktop header

--- src/components/sections/header/header.tsx
+++ src/components/sections/header/header.tsx
@@ -1,33 +1,48 @@
-"use client";
+'use client';
 
-import { AnimatePresence } from "framer-motion";
-import Burger from "./burger";
-import Container from "@/components/layout/container";
-import Image from "next/image";
-import Logo from "../logo";
-import { motion } from "framer-motion";
-import { useState } from "react";
+import { AnimatePresence } from 'framer-motion';
+import Burger, { headerLinks } from './burger';
+import Container from '@/components/layout/container';
+import Image from 'next/image';
+import Logo from '../logo';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import LinkExternal from '../link-external';
+import BurgerLinkInternal from './burger-internal-link';
+import LinkInternal from '../link-internal';
+import { Button } from '@/components/ui/button';
 
 const Header = () => {
   const [isBurgerOpened, setIsBurgerOpened] = useState(false);
+
   return (
     <header className="header">
       <Burger isOpened={isBurgerOpened} />
       <Container className="flex items-center gap-6 justify-between pt-6">
         <Logo />
-        <button className="size-6 relative" onClick={() => setIsBurgerOpened(!isBurgerOpened)}>
+
+        <ul className="flex items-center gap-10">
+          {headerLinks.map((link, i) => (
+            <li key={i}>
+              <LinkInternal {...link} />
+            </li>
+          ))}
+        </ul>
+
+        <Button className="font-medium text-[18px] px-10">Get started</Button>
+        <button
+          className="size-6 relative lg:hidden"
+          onClick={() => setIsBurgerOpened(!isBurgerOpened)}>
           <motion.div
             className="origin-bottom fill-mode-forwards w-6 absolute bottom-0 left-0"
             initial={{ opacity: 1, scale: 1 }}
-            animate={isBurgerOpened ? { opacity: 0, scale: 0 } : {}}
-          >
+            animate={isBurgerOpened ? { opacity: 0, scale: 0 } : {}}>
             <Image src="/images/menu.svg" alt="Menu" width={24} height={24} />
           </motion.div>
           <motion.div
             className="origin-top fill-mode-forwards w-6 absolute top-0 left-0"
             initial={{ opacity: 0, scale: 0 }}
-            animate={isBurgerOpened ? { opacity: 1, scale: 1 } : {}}
-          >
+            animate={isBurgerOpened ? { opacity: 1, scale: 1 } : {}}>
             <Image src="/images/menu-close.svg" alt="Menu" width={24} height={24} />
           </motion.div>
         </button>
