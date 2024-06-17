3f74eadffb2529c973c8a34d4f4b8b602d5e72f8
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-17 18:39:30 +0500

    for tabl

--- src/components/sections/footer/footer-logo.tsx
+++ src/components/sections/footer/footer-logo.tsx
@@ -1,13 +1,15 @@
-import { general } from "@/config/general";
+import { general } from '@/config/general';
 
 const FooterLogo = () => {
   const data = new Date();
   return (
-    <div className="footer-logo flex flex-col gap-2 items-center">
-      <a href="#" className="text-2xl uppercase font-semibold">
+    <div className="footer-logo flex flex-col gap-2 sm:gap-6 items-center sm:items-start">
+      <a href="#" className="text-2xl sm:text-5xl uppercase font-semibold">
         {general.appName}
       </a>
-      <p className="text-GRAY text-sm">{`© ${general.appName} ${data.getFullYear()}`}</p>
+      <p className="text-GRAY text-sm sm:text-[18px] sm:leading-none sm:font-bold sm:text-black">{`©  ${
+        general.appName
+      } ${data.getFullYear()}`}</p>
     </div>
   );
 };
