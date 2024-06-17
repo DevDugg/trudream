fadb7149cef92d08ad96606299d1b6f4f843dbc3
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-06-17 20:27:54 +0900

    border color animated

--- src/components/sections/questions/questions.tsx
+++ src/components/sections/questions/questions.tsx
@@ -1,11 +1,13 @@
-'use client';
+"use client";
 
-import { AnimatePresence, motion } from 'framer-motion';
-import Container from '@/components/layout/container';
-import { questionsData } from '@/data/questions.data';
-import Image from 'next/image';
-import { useState } from 'react';
-import clsx from 'clsx';
+import { AnimatePresence, motion } from "framer-motion";
+
+import Container from "@/components/layout/container";
+import Image from "next/image";
+import clsx from "clsx";
+import { colors } from "@/config/colors";
+import { questionsData } from "@/data/questions.data";
+import { useState } from "react";
 
 const Questions = () => {
   const [activeFAQ, setActiveFAQ] = useState(0);
@@ -18,26 +20,25 @@ const Questions = () => {
         <div className="flex flex-col gap-6">
           {questionsData.map((item, i) => (
             <motion.div
-              initial={{ height: 0 }}
-              animate={{ height: '100%' }}
+              initial={{ height: 0, borderColor: colors.GRAY }}
+              animate={{ height: "100%", borderColor: i === activeFAQ ? `${colors.GRAY}00` : colors.GRAY }}
               exit={{ height: 0 }}
               onClick={() => setActiveFAQ(i)}
               key={item.title}
-              className={clsx(
-                'flex flex-col text-BLACK rounded-[16px] transition-all cursor-pointer bg-LIGHT_GRAY border-GRAY border-[1px] font-semibold leading-[120%] py-[18.5px] px-4',
-                {
-                  'border-none': activeFAQ === i,
-                },
-              )}>
+              className={
+                "flex flex-col text-BLACK rounded-[16px] cursor-pointer border bg-LIGHT_GRAY font-semibold leading-[120%] py-[18.5px] px-4"
+              }
+            >
               <div className="flex items-center gap-6 justify-between">
                 <h4
-                  className={clsx('transition-all', {
-                    'text-ACCENT': activeFAQ === i,
-                  })}>
+                  className={clsx({
+                    "text-ACCENT": activeFAQ === i,
+                  })}
+                >
                   {item.title}
                 </h4>
                 <Image
-                  src={`/images/${activeFAQ === i ? 'minus' : 'plus'}.svg`}
+                  src={`/images/${activeFAQ === i ? "minus" : "plus"}.svg`}
                   alt="plus"
                   width={24}
                   height={24}
@@ -50,13 +51,12 @@ const Questions = () => {
                   <motion.div
                     key="content"
                     initial={{ opacity: 0, height: 0 }}
-                    animate={{ opacity: 1, height: 'auto' }}
+                    animate={{ opacity: 1, height: "auto" }}
                     exit={{ opacity: 0, height: 0 }}
                     transition={{ duration: 0.3 }}
-                    className="overflow-hidden">
-                    <p className="mt-6 text-[16px] transition-all leading-[140%] font-normal">
-                      {item.text}
-                    </p>
+                    className="overflow-hidden"
+                  >
+                    <p className="mt-6 text-[16px] transition-all leading-[140%] font-normal">{item.text}</p>
                   </motion.div>
                 )}
               </AnimatePresence>
--- src/config/colors.ts
+++ src/config/colors.ts
@@ -1,12 +1,12 @@
 export const colors: Record<string, `#${string}` | `rgb(${string})` | `rgba(${string})`> = {
-  ACCENT: "rgba(13, 32, 96, 1)",
-  SECONDARY: "rgba(253, 248, 251, 1)",
-  BACKGROUND: "rgba(255, 253, 252, 1)",
-  PINK: "rgba(255, 200, 221, 1)",
-  PINK_HOVER: "rgba(255, 200, 221, 1)",
-  BLACK: "rgba(23, 23, 23, 1)",
-  OPAQUE_BLACK: "rgba(23, 23, 23, 0.6)",
-  GRAY: "rgba(204, 204, 204, 1)",
-  LIGHT_GRAY: "rgba(245, 245, 245, 1)",
-  WHITE: "rgba(255, 255, 255, 1)",
+  ACCENT: "#0D2060",
+  SECONDARY: "#FDF8FB",
+  BACKGROUND: "#FFFDFC",
+  PINK: "#FFC8DD",
+  PINK_HOVER: "#FFC8DD",
+  BLACK: "#171717",
+  OPAQUE_BLACK: "rgba(23, 23, 23, 0.6)", // No direct hex equivalent due to opacity
+  GRAY: "#CCCCCC",
+  LIGHT_GRAY: "#F5F5F5",
+  WHITE: "#FFFFFF",
 };
