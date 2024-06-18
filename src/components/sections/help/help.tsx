7c250a30ba33eef8fc070553940923df642e7873
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-18 19:57:27 +0500

    lg is ready

--- src/components/sections/help/help.tsx
+++ src/components/sections/help/help.tsx
@@ -3,18 +3,22 @@ import HeroForm from '../hero/hero-form';
 
 const Help = () => {
   return (
-    <section className="bg-HELP_PATTERN">
-      <Container className="py-10 flex flex-col gap-10">
-        <div>
-          <h3 className="h3 mb-4">Need some help to build solutions that drive changes?</h3>
-          <p className="p">
+    <section className="bg-HELP_PATTERN w-full sm:bg-none">
+      <Container className="py-10 flex flex-col xl:flex-row xl:justify-between xl:items-center gap-10 xl:gap-20 sm:bg-HELP_PATTERN">
+        <div className="">
+          <h3 className="h3 mb-4 xl:mb-10">
+            Need some help to build solutions that drive changes?
+          </h3>
+          <p className="p lg:text-[20px]">
             We empowers business owners like you to turn dreams into reality. We provide funding and
             financial literacy education to help you launch, scale, or invest in your business
             goals.
           </p>
         </div>
 
-        <HeroForm />
+        <div className="flex-shrink-0">
+          <HeroForm />
+        </div>
       </Container>
     </section>
   );
