f21152f1217df27a6ce6632f5dbbd6bbe1e6713b
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-18 17:54:06 +0500

    changed key

--- src/components/sections/reviews/reviews.tsx
+++ src/components/sections/reviews/reviews.tsx
@@ -10,10 +10,8 @@ const Reviews = () => {
         <h3 className="h3 -tracking-[1%] mb-10 text-center">What clients say about us</h3>
 
         <div className="flex flex-col sm:flex-row flex-nowrap sm:flex-wrap items-center justify-center gap-6">
-          {reviewsData.map((item) => (
-            <div
-              key={item.name}
-              className={clsx('p-4 bg-LIGHT_GRAY rounded-[24px] w-full max-w-[410px]')}>
+          {reviewsData.map((item, i) => (
+            <div key={i} className={clsx('p-4 bg-LIGHT_GRAY rounded-[24px] w-full max-w-[410px]')}>
               <div className="flex flex-col gap-1 mb-3">
                 <h5 className="text-[18px] leading-none font-bold text-BLACK -tracking-[1%]">
                   {item.name}
