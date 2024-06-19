a6ecb05a1964be470b96c03d4ad20407c4a9cdaf
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-06-20 03:11:05 +0900

    hover animations done through animate presence

--- src/components/sections/business/hover-items.tsx
+++ src/components/sections/business/hover-items.tsx
@@ -1,12 +1,14 @@
 "use client";
 
+import { AnimatePresence, motion } from "framer-motion";
 import { useRef, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { businessData } from "@/data/business.data";
+import clsx from "clsx";
 import { colors } from "@/config/colors";
-import { motion } from "framer-motion";
+import { defaultTransition } from "@/config/transitions";
 
 const HoverItems = () => {
   const [hoverItem, setHoverItem] = useState(0);
@@ -15,20 +17,52 @@ const HoverItems = () => {
   return (
     <>
       <div className="flex justify-between">
-        <motion.div
-          initial={{ top: 0, opacity: 0 }}
-          animate={{
-            top: hoverItem * 128,
-            opacity: [0, 1],
-          }}
-          className="flex-[0_0_300px] top-0 relative xl:mx-auto"
-        >
-          <div className="absolute">
-            <Image src={`/images/business/${businessData[hoverItem].img}.png`} alt="plus" width={300} height={170} />
-            <p className="p mt-4 mb-6">{businessData[hoverItem].text}</p>
-            <Button className="w-full font-medium">Book a call</Button>
-          </div>
-        </motion.div>
+        <div className="relative max-w-[300px] w-full">
+          <motion.div
+            className="flex-col left-0 w-full absolute xl:mx-auto"
+            initial={{ top: 0 }}
+            animate={{ top: hoverItem * 128 }}
+          >
+            <div>
+              <div className="overflow-hidden h-[170px] relative">
+                <AnimatePresence mode="popLayout">
+                  {businessData.map(
+                    (item, i) =>
+                      i === hoverItem && (
+                        <motion.div
+                          className={"absolute"}
+                          key={i}
+                          initial={i === 0 ? { top: "0%" } : { top: "100%" }}
+                          animate={i === hoverItem ? { top: "0%", scale: 1 } : {}}
+                          exit={{ top: "100%", scale: 0.4 }}
+                          transition={defaultTransition}
+                        >
+                          <Image src={`/images/business/${item.img}.png`} alt={item.title} width={300} height={300} />
+                        </motion.div>
+                      ),
+                  )}
+                </AnimatePresence>
+              </div>
+              <AnimatePresence mode="wait">
+                {businessData.map(
+                  (item, i) =>
+                    i === hoverItem && (
+                      <motion.div
+                        key={i}
+                        initial={{ opacity: 0 }}
+                        animate={i === hoverItem ? { opacity: 1 } : {}}
+                        exit={{ opacity: 0 }}
+                        transition={{ duration: 0.3 }}
+                      >
+                        <p className="p mt-4 mb-6">{businessData[hoverItem].text}</p>
+                        <Button className="w-full font-medium">Book a call</Button>
+                      </motion.div>
+                    ),
+                )}
+              </AnimatePresence>
+            </div>
+          </motion.div>
+        </div>
 
         <motion.div
           className="flex flex-col flex-[0_1_50%]"
