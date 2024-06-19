fd9d89ba31af931c33135546f47ec040663e3d6b
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-19 19:49:38 +0500

    slice to seperate comp

--- src/components/sections/questions/questions.tsx
+++ src/components/sections/questions/questions.tsx
@@ -1,16 +1,7 @@
-'use client';
-
-import { motion } from 'framer-motion';
-
 import Container from '@/components/layout/container';
-import Image from 'next/image';
-import { colors } from '@/config/colors';
-import { questionsData } from '@/data/questions.data';
-import { useState } from 'react';
+import QuestionClientBlock from '../guide/question-client-block';
 
 const Questions = () => {
-  const [activeFAQ, setActiveFAQ] = useState(0);
-
   return (
     <section>
       <Container>
@@ -18,51 +9,7 @@ const Questions = () => {
           Have any questions?
         </h3>
 
-        <div className="flex flex-col gap-6">
-          {questionsData.map((item, i) => (
-            <motion.div
-              initial={{ borderColor: colors.GRAY }}
-              animate={{ borderColor: i === activeFAQ ? `${colors.GRAY}00` : colors.GRAY }}
-              onClick={() => setActiveFAQ(i)}
-              key={item.title}
-              className={
-                'flex flex-col text-BLACK rounded-[16px] cursor-pointer border bg-LIGHT_GRAY font-semibold leading-[120%] py-[18.5px] px-4'
-              }>
-              <div className="flex items-center gap-6 justify-between">
-                <motion.h4
-                  initial={{ color: colors.BLACK }}
-                  animate={i === activeFAQ ? { color: colors.ACCENT } : {}}
-                  className="text-[18px] lg:text-[24px] leading-[120%] font-semibold ">
-                  {item.title}
-                </motion.h4>
-                <div className="relative size-6">
-                  <motion.div
-                    className="top-0 right-0 size-6 absolute"
-                    initial={{ opacity: 1 }}
-                    animate={i === activeFAQ ? { opacity: 0 } : {}}>
-                    <Image src={`/images/plus.svg`} alt="plus" width={24} height={24} />
-                  </motion.div>
-                  <motion.div
-                    className="top-0 right-0 size-6 absolute"
-                    initial={{ opacity: 0 }}
-                    animate={i === activeFAQ ? { opacity: 1 } : {}}>
-                    <Image src={`/images/minus.svg`} alt="minus" width={24} height={24} />
-                  </motion.div>
-                </div>
-              </div>
-
-              <motion.div
-                key="content"
-                initial={{ opacity: 0, height: 0, marginTop: 0 }}
-                animate={
-                  i === activeFAQ ? { opacity: 1, height: 'fit-content', marginTop: 16 } : {}
-                }
-                className="overflow-hidden">
-                <p className="p font-normal">{item.text}</p>
-              </motion.div>
-            </motion.div>
-          ))}
-        </div>
+        <QuestionClientBlock />
       </Container>
     </section>
   );
