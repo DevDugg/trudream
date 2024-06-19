4f77e3b99546aff630aeaa71d8b923571acc9927
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-19 19:47:23 +0500

    client component is seperate

--- src/components/sections/business/business.tsx
+++ src/components/sections/business/business.tsx
@@ -1,19 +1,13 @@
-'use client';
-
 import { Button } from '@/components/ui/button';
 import Container from '@/components/layout/container';
 import Image from 'next/image';
 import { businessData } from '@/data/business.data';
-import { useState } from 'react';
-import { motion } from 'framer-motion';
-import { colors } from '@/config/colors';
+import HoverItems from './hover-items';
 
 const Business = () => {
-  const [hoverItem, setHoverItem] = useState(0);
-
   return (
     <section>
-      <Container>
+      <Container className="pb-[330px]">
         <div className="flex lg:hidden flex-col gap-10">
           <h3 className="h3 sm:text-center">Funding Solutions & Financial Education by TruDream</h3>
 
@@ -43,52 +37,7 @@ const Business = () => {
             Funding Solutions & Financial Education by TruDream
           </h3>
 
-          <div className="flex justify-between">
-            <motion.div
-              initial={{ top: 0, opacity: 0 }}
-              animate={{
-                top: hoverItem * 128,
-                opacity: 1,
-              }}
-              className="flex-[0_0_300px] top-0 relative xl:mx-auto">
-              <div className="absolute">
-                <Image
-                  src={`/images/business/${businessData[hoverItem].img}.png`}
-                  alt="plus"
-                  width={300}
-                  height={170}
-                />
-                <p className="p mt-4 mb-6">{businessData[hoverItem].text}</p>
-                <Button className="w-full font-medium">Book a call</Button>
-              </div>
-            </motion.div>
-
-            <div className="flex flex-col flex-[0_1_50%]">
-              {businessData.map((item, i) => (
-                <motion.div
-                  initial={{
-                    backgroundColor: `${colors.LIGHT_GRAY}00`,
-                    paddingLeft: 0,
-
-                    color: colors.GRAY,
-                  }}
-                  animate={
-                    i === hoverItem
-                      ? {
-                          backgroundColor: colors.LIGHT_GRAY,
-                          paddingLeft: '24px',
-                          color: colors.ACCENT,
-                        }
-                      : {}
-                  }
-                  key={i}
-                  onMouseMove={() => setHoverItem(i)}
-                  className="!py-10 text-[48px] font-bold leading-none !pr-6 rounded-2xl">
-                  {item.title}
-                </motion.div>
-              ))}
-            </div>
-          </div>
+          <HoverItems />
         </div>
       </Container>
     </section>
---
+++ src/components/sections/business/hover-items.tsx
@@ -0,0 +1,74 @@
+'use client';
+
+import Image from 'next/image';
+import { useRef, useState } from 'react';
+import { motion } from 'framer-motion';
+import { colors } from '@/config/colors';
+import { Button } from '@/components/ui/button';
+import { businessData } from '@/data/business.data';
+
+const HoverItems = () => {
+  const [hoverItem, setHoverItem] = useState(0);
+  const titleRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <>
+      <div className="flex justify-between">
+        <motion.div
+          initial={{ top: 0, opacity: 0 }}
+          animate={{
+            top: hoverItem * 128,
+            opacity: [0, 1],
+          }}
+          className="flex-[0_0_300px] top-0 relative xl:mx-auto">
+          <div className="absolute">
+            <Image
+              src={`/images/business/${businessData[hoverItem].img}.png`}
+              alt="plus"
+              width={300}
+              height={170}
+            />
+            <p className="p mt-4 mb-6">{businessData[hoverItem].text}</p>
+            <Button className="w-full font-medium">Book a call</Button>
+          </div>
+        </motion.div>
+
+        <div className="flex flex-col flex-[0_1_50%]">
+          {businessData.map((item, i) => (
+            <motion.div
+              ref={titleRef}
+              initial={{
+                backgroundColor: `${colors.LIGHT_GRAY}00`,
+                paddingLeft: 0,
+                borderRadius: 0,
+                borderColor: colors.GRAY,
+                borderTop: 1,
+                borderBottom: 1,
+                color: colors.GRAY,
+              }}
+              animate={
+                i === hoverItem
+                  ? {
+                      backgroundColor: colors.LIGHT_GRAY,
+                      paddingLeft: '24px',
+                      color: colors.ACCENT,
+                      borderRadius: 16,
+                      borderTop: 0,
+                      borderBottom: 0,
+                      borderColor: colors.GRAY + '00',
+                    }
+                  : {}
+              }
+              key={i}
+              onMouseMove={() => setHoverItem(i)}
+              className="!py-10 cursor-pointer text-[48px] font-bold leading-none !pr-6">
+              {item.title}
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HoverItems;
