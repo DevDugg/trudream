2877c03d3277789e3f284f747a2669bbae6dc474
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-19 19:49:19 +0500

    from lg to xl desktop -v

--- src/components/sections/hero/hero.tsx
+++ src/components/sections/hero/hero.tsx
@@ -7,18 +7,18 @@ import { heroPayData } from '@/data/hero.data';
 const Hero = () => {
   return (
     <section className="hero bg-HERO_PATTERN">
-      <Container className="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-10 sm:gap-8">
-        <div className="flex flex-col gap-4 sm:text-center lg:text-left lg:max-w-[690px]">
-          <h1 className="text-[32px] sm:text-[40px] lg:text-[80px] font-bold leading-none lg:mb-10">
+      <Container className="flex flex-col xl:flex-row xl:items-center xl:justify-between gap-10 sm:gap-8">
+        <div className="flex flex-col gap-4 sm:text-center xl:text-left xl:max-w-[690px]">
+          <h1 className="text-[32px] sm:text-[40px] lg:text-[80px] font-bold leading-none xl:mb-10">
             {general.appDescription}
           </h1>
-          <p className="text-BLACK text-base sm:text-[18px] lg:text-[20px] lg:max-w-[590px] leading-[140%] lg:mb-20">
+          <p className="text-BLACK text-base sm:text-[18px] lg:text-[20px] xl:max-w-[590px] leading-[140%] xl:mb-20">
             We empowers business owners like you to turn dreams into reality. We provide funding and
             financial literacy education to help you launch, scale, or invest in your business
             goals.
           </p>
 
-          <div className="hidden lg:block">
+          <div className="hidden xl:block">
             <h4 className="h4 mb-4">Invested in</h4>
             <div className="flex gap-5">
               {heroPayData.map((item) => (
@@ -37,7 +37,7 @@ const Hero = () => {
         <div className="flex-[0_0_40%]">
           <HeroForm />
 
-          <div className="flex flex-col lg:hidden sm:items-center gap-4 sm:gap-6">
+          <div className="flex flex-col xl:hidden sm:items-center gap-4 sm:gap-6">
             <h4 className="h4">Invested in</h4>
             <div className="flex gap-5">
               {heroPayData.map((item) => (
