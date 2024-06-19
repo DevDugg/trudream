77fe0cb77538e3cb30feffc7e0f22f6e770eacda
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-19 19:47:41 +0500

    card seperate component

--- src/components/sections/about-us/about-us.tsx
+++ src/components/sections/about-us/about-us.tsx
@@ -2,6 +2,8 @@ import Container from '@/components/layout/container';
 import { Button } from '@/components/ui/button';
 import { aboutData } from '@/data/about-us.data';
 import Image from 'next/image';
+import AboutCarf from './about-card';
+import AboutCard from './about-card';
 
 const AboutUs = () => {
   return (
@@ -29,14 +31,7 @@ const AboutUs = () => {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-10 sm:gap-6">
           {aboutData.map((item) => (
-            <div key={item.title} className="p-6 sm:py-4 bg-LIGHT_GRAY rounded-[16px]">
-              <div className="flex items-center gap-4 mb-3">
-                <Image src={`/images/about/${item.img}.svg`} alt="icon" width={32} height={32} />
-                <h4 className="text-ACCENT leading-none text-[20px] font-semibold">{item.title}</h4>
-              </div>
-
-              <p className="text-BLACK leading-[140%] text-[18px]">{item.text}</p>
-            </div>
+            <AboutCard key={item.title} {...item} />
           ))}
         </div>
       </Container>
