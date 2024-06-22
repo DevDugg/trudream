ea96eb4f00924b5594787c2ccf91e65601c1ece9
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-06-22 17:14:53 +0500

    deleted import

--- src/components/sections/about-us/about-card.tsx
+++ src/components/sections/about-us/about-card.tsx
@@ -1,7 +1,6 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import { motion } from "framer-motion";
+import Image from 'next/image';
 
 interface IProps {
   title: string;
@@ -13,18 +12,10 @@ const AboutCard = (props: IProps) => {
   return (
     <div
       key={props.title}
-      className="p-6 bg-LIGHT_GRAY rounded-[16px] hover:-translate-y-3 transition-all duration-500 cursor-default"
-    >
+      className="p-6 bg-LIGHT_GRAY rounded-[16px] hover:-translate-y-3 transition-all duration-500 cursor-default">
       <div className="flex items-center gap-4 mb-3">
-        <Image
-          src={`/images/about/${props.img}.svg`}
-          alt="icon"
-          width={32}
-          height={32}
-        />
-        <h4 className="text-ACCENT leading-none text-[20px] font-semibold">
-          {props.title}
-        </h4>
+        <Image src={`/images/about/${props.img}.svg`} alt="icon" width={32} height={32} />
+        <h4 className="text-ACCENT leading-none text-[20px] font-semibold">{props.title}</h4>
       </div>
 
       <p className="text-BLACK leading-[140%] text-[18px]">{props.text}</p>
