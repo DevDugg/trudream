3ddf0f6fd22d940e6c94a09fd5bbb62c46e45eba
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-06-26 19:47:22 +0900

    dangerouslyAllowSVG

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["cdn.sanity.io"],
+    dangerouslyAllowSVG: true,
   },
   env: {
     NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
