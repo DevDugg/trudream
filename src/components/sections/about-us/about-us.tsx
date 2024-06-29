c0c228d598eb46e66a3d20e25f96264956d3e015
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-06-29 16:23:49 +0900

    REMOVED: clg

--- src/components/sections/about-us/about-us.tsx
+++ src/components/sections/about-us/about-us.tsx
@@ -7,28 +7,18 @@ import { getAboutSectionData } from "../../../../sanity/schemas/about";
 const AboutUs = async () => {
   const aboutSectionData = await getAboutSectionData();
 
-  console.log("first");
-
   return (
     <section id="about">
       <Container>
         <div className="flex flex-col lg:flex-row lg:justify-between gap-4 mb-10 lg:mb-12">
-          <h3 className="h3 lg:max-w-[600px] lg:flex-[0_1_610px]">
-            {aboutSectionData[0].title}
-          </h3>
+          <h3 className="h3 lg:max-w-[600px] lg:flex-[0_1_610px]">{aboutSectionData[0].title}</h3>
 
           <div className="lg:flex-[0_1_480px]">
-            <RichText className="p lg:mb-8">
-              {aboutSectionData[0].subtitle}
-            </RichText>
-            <Button className="hidden lg:block px-10 text-[18px] font-medium mb-10">
-              Browse our services
-            </Button>
+            <RichText className="p lg:mb-8">{aboutSectionData[0].subtitle}</RichText>
+            <Button className="hidden lg:block px-10 text-[18px] font-medium mb-10">Browse our services</Button>
           </div>
         </div>
-        <Button className="w-full lg:hidden text-[18px] font-medium mb-10">
-          Browse our services
-        </Button>
+        <Button className="w-full lg:hidden text-[18px] font-medium mb-10">Browse our services</Button>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-10 sm:gap-6">
           {aboutSectionData[0].cards.map((item, i) => (
